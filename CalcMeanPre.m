7bc5a1566db77677d9a8b840ddfb9ccf6054e11a
Jonas Rud Bjørndal <153197565+JONAS-RUD-BJORNDAL@users.noreply.github.com>
2024-02-28 11:18:22 +0100

    Delete CalcMeanPre.m

--- CalcMeanPre.m
+++
@@ -1,9 +0,0 @@
-DataForMean = data{1, 1}.experimentData.visualFeedbackShown %% extract data from structure
-
-DataForMean(:,1)=[] %% Remove columns without values, replace X with # column (e.g. (1,1) if column 1 is missing. 
-                       %%If mulitple, then add them as vector, e.g. (:,[1 3])
-
-DataConverted = cell2mat(DataForMean) %% Extract as cells - not structure
-
-MeanIs = mean(DataConverted(1:2:end))
-
